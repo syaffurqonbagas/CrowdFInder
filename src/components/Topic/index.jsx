b27528f7edc573b49bb66defc12b59958f2d4e54
inokiardhi <ardhiinoki@gmail.com>
2021-08-13 23:23:16 +0800

    createPost, SmallCardMyEvent, Topic done

---
+++ src/components/Topic/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Button, Card, Container, ListGroup } from 'react-bootstrap'
+
+
+function Topic() {
+    return (
+        <>
+            <Card style={{ width: '18rem' }}>
+                <ListGroup variant="flush">
+                    <ListGroup.Item className="part-1 mt-2">
+                        <h6>What topic would you like to see now?</h6>
+                        <p>You will only see topics you selected. Go to Account menu to add more topics.</p>
+                        <div>
+                            <div className="d-flex mb-1">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                <label class="form-check-label">Social</label>
+                            </div>
+                            <div className="d-flex mb-1">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                <label class="form-check-label">Business</label>
+                            </div>
+                            <div className="d-flex mb-1">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                <label class="form-check-label">Politics</label>
+                            </div>
+                            <div className="d-flex mb-1">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                <label class="form-check-label">Art</label>
+                            </div>
+                        </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item className="part-2  mb-3">
+                        <h6>Which events would you like to see?</h6>
+                        <div>
+                            <div className="d-flex mb-1">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                <label class="form-check-label">Events near me</label>
+                            </div>
+                            <div className="d-flex mb-5">
+                                <input class="form-check-input me-1" type="checkbox" value=""/>
+                                <label class="form-check-label">Events from community I’ve followed</label>
+                            </div>
+                        </div>
+                        
+                        <Button className="d-block mx-auto" style={{ width: '15rem' }}>Update</Button>
+                    </ListGroup.Item>
+                </ListGroup>
+                
+            </Card>
+        </>
+    )
+}
+
+export default Topic
