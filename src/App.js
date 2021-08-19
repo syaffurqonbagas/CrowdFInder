e432015093f90a87d3624b5095966a1555c3638d
inokiardhi <ardhiinoki@gmail.com>
2021-08-19 15:23:37 +0800

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import { BrowserRouter } from "react-router-dom"
 import Routers from "./routes/route"
-import CreateCommunityForm from "./components/CreateCommunityForm";
-import HomePage from "./pages/HomePage";
 
 function App() {
   return (
 
     <BrowserRouter>
-      {/* <Routers /> */}
-      {/* <CreateCommunityForm/> */}
-      {/* <FormCreatePost/> */}
-      {/* <FormCreateEvent/> */}
-      <HomePage/>
+      <Routers />
     </BrowserRouter>
   );
 }
