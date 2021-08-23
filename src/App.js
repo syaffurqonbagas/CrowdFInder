21583385902429a62f50f33e42293432ed020736
inokiardhi <ardhiinoki@gmail.com>
2021-08-23 17:34:18 +0800

    adding form function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import Routers from "./routes/route"
+import CreateCommunityForm from "./components/CreateCommunityForm"
 
 function App() {
   return (
--- src/components/LargeCardMyEvent/LargeCardMyEvent.jsx
+++ src/components/LargeCardMyEvent/LargeCardMyEvent.jsx
@@ -20,11 +20,11 @@ function LargeCardMyEvent() {
 
     return (
         <>
-            <div className="divider my-3 mb-3"></div>
+            <div className="divider mt-5 mb-5"></div>
             <div className="headContainer">
 
                 <div className="d-flex">
-                    <div className="imageAvatar mb-2">
+                    <div className="imageAvatar mb-4 me-2">
                         <img src={user}/>
                     </div>
                     <div className="headText container-fluid d-block mb-2">
@@ -43,9 +43,9 @@ function LargeCardMyEvent() {
                         </div>
 
                         <div className="headTextMain d-flex align-content-center">
-                            <label className="my-auto" style={{ fontSize: '20px', fontWeight: '700' }}>Inoki Ardhi</label>
-                            <label className="headTextBadge rounded-pill ms-3 me-auto">Design</label>
-                            <label style={{ fontSize: '18px', fontWeight: '400', color: '#828282' }} ><i class="fas fa-map-marker-alt ms-auto me-0"></i> Sumbawa</label>
+                            <label className="my-auto" style={{ fontSize: '20px', fontWeight: '400' }}>Inoki Ardhi</label>
+                            <label className="headTextBadge rounded-pill ms-3 me-auto" style={{ fontSize: '16px', fontWeight: '400' }}>Design</label>
+                            <label style={{ fontSize: '18px', fontWeight: '400', color: '#828282' }} ><i class="fas fa-map-marker-alt fa-xs ms-auto me-0"></i> Sumbawa</label>
                         </div>
                         <label style={{ fontSize: '16px', fontWeight: '400', color: '#4F4F4F' }}>1 hours ago</label>
                     </div>
@@ -54,13 +54,13 @@ function LargeCardMyEvent() {
                 <div>
                     <Card>
                         <div className="w-75 ms-3 mt-3 mb-4">
-                            <p className="fontSize">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed quaerat commodi reprehenderit similique ad soluta omnis nesciunt minima, sequi numquam et officia suscipit iusto minus libero ea reiciendis distinctio odit?</p>
+                            <p className="fontSize" style={{ fontSize: '14px', fontWeight: '400' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed quaerat commodi reprehenderit similique ad soluta omnis nesciunt minima, sequi numquam et officia suscipit iusto minus libero ea reiciendis distinctio odit?</p>
                             <img className="imageSize" src={image} alt="" />
                         </div>
 
                         <div className="btnGroup d-inline-flex">
-                            <button className="button-card flex-grow-1"><i class="fas fa-thumbs-up"></i>Like(10)</button>
-                            <button className="button-card flex-grow-1"><i class="fas fa-comment-dots"></i>Comment(15)</button>
+                            <button className="button-card flex-grow-1"><i class="fas fa-thumbs-up"></i>Like (10)</button>
+                            <button className="button-card flex-grow-1"><i class="fas fa-comment-dots"></i>Comment (15)</button>
                             <button className="button-card flex-grow-1"><i class="fas fa-share-alt"></i>Share</button>
                         </div>
 
