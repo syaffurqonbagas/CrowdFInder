ae7335f0f6a506e42c85c87ee369acae8d3471fe
Bagas-Syaffurqon-L20010174 <syaffurqonbagas@gmail.com>
2021-08-30 22:28:19 +0700

    Add detail profile

--- src/components/Bio/index.jsx
+++ src/components/Bio/index.jsx
@@ -3,35 +3,10 @@ import './index.scss'
 import { Form, Button } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal'
 
-function Bio() {
-    const ref = useRef();
-    useOnClickOutside(ref, () => setShow(false));
+function Bio(props) {
+
     const [show, setShow] = useState(false);
 
-    function useOnClickOutside(ref, handler) {
-        useEffect(
-            () => {
-                const listener = (event) => {
-                    if (!ref.current || ref.current.contains(event.target)) {
-                        return;
-                    }
-                    handler(event);
-                };
-                document.addEventListener("mousedown", listener);
-                document.addEventListener("touchstart", listener);
-                return () => {
-                    document.removeEventListener("mousedown", listener);
-                    document.removeEventListener("touchstart", listener);
-                };
-            },
-
-            [ref, handler]
-        );
-    }
-
-    const toggleDropDown = () => {
-        setShow(!show);
-    }
 
     return (
 
@@ -39,30 +14,33 @@ function Bio() {
             <div className="title d-flex justify-content-between align-items-center">
                 <h5>Bio</h5>
 
-                <div ref={ref} className="my-auto d-flex position-relative">
+                <div className="my-auto d-flex position-relative">
                     <i className="button-edit fa fa-edit"
-                        onClick={() => toggleDropDown()}
-                        tabIndex="0"></i>
-                    {show && (
-                        <Form className='position-absolute modal-edit'>
-                            <div className="modal-body">
-                                <Form.Group className="mb-2" controlId="exampleForm.ControlTextarea1">
-                                    <Form.Label style={{ fontWeight: '700' }}>Bio Edit</Form.Label>
-                                    <Form.Control className='modal-textarea' as="textarea" />
-                                </Form.Group>
-                                <Button variant="secondary" type="submit">
-                                    save
-                                </Button>
-                            </div>
-                        </Form>
-                    )}
+                        onClick={() => setShow(true)}
+                    ></i>
+
                 </div>
             </div>
             <div className="body mt-2">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tortor neque porta odio tellus. Vulputate ut sit leo nunc ipsum lacus egestas mi aliquet. Interdum morbi leo cursus ultrices augue nulla est. Pulvinar nunc non vulputate sed nunc amet. At dignissim enim maecenas ut odio.</p>
+                <p>{props.bio}</p>
             </div>
+            {show ?
+                <Form className='modal-edit'>
+                    <Form.Group className="my-4" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label style={{ fontWeight: '700' }}>Bio Edit</Form.Label>
+                        <Form.Control className='modal-textarea' as="textarea" />
+                    </Form.Group>
+                    <div className="button">
+                        <Button className='me-3' variant="primary" type="submit">
+                            save
+                        </Button>
+                        <Button variant="secondary" onClick={() => setShow(false)}>
+                            Cancel
+                        </Button>
+                    </div>
+                </Form>
+                : null}
         </div >
-
     )
 }
 
--- src/pages/MyProfile/about.jsx
+++ src/pages/MyProfile/about.jsx
@@ -4,17 +4,21 @@ import InterstTopic from '../../components/InterestTopic';
 import ListCardPeople from '../../components/ListCardPeople';
 import { Link } from "react-router-dom"
 import './index.scss'
+import { useSelector } from 'react-redux';
+import { Badge } from 'react-bootstrap';
 
 function About(props) {
+    const user = useSelector((state) => state.userData.user)
+
     return (
         <div>
-            <Bio />
+            <Bio bio={user.bio} />
             <hr />
-            <InterstTopic />
+            <InterstTopic list={user.interest} />
             <hr />
             <div className="myCrowd d-flex justify-content-between py-3">
                 <h5>My Crowd</h5>
-                <h5 >  <Link style={{ color: '#D82671', fontWeight: '400', textDecoration: 'none', fontSize: '16px' }} to="/">See All</Link></h5>
+                <h5 >  <Link style={{ color: '#D82671', fontWeight: '400', textDecoration: 'none', fontSize: '16px' }} to="/mycrowd">See All</Link></h5>
             </div>
             <ListCardPeople />
         </div>
