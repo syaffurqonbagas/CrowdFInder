32f9be7365864fd1a9636c707619edd131cc0a2e
Bima Gofarali Roby Kusuma Harta <bimagofaralirobi@gmail.com>
2021-09-04 11:49:47 +0700

    fix reducer

--- src/redux/saga/index.js
+++ src/redux/saga/index.js
@@ -1,17 +1,23 @@
 import { all } from "@redux-saga/core/effects";
-import { watchPostAnnouncements, watchUpdateAnnouncement } from "./announcement";
+import {
+  watchPostAnnouncements,
+  watchUpdateAnnouncement,
+} from "./announcement";
 import { watchGetComments, watchPostComments } from "./comment";
 import { watchPostEvents } from "./event";
 import { watchDeletePosts, watchGetPosts } from "./post";
 import { watchGetPostById } from "./postById";
-import { watchRegister, watchLogin, watchLogout, watchGetUser, watchUserUpdate } from "./user";
+import {
+  watchRegister,
+  watchLogin,
+  watchLogout,
+  watchGetUser,
+  watchUserUpdate,
+} from "./user";
 import { watchPutLikes } from "./like";
-<<<<<<< HEAD
-import {watchSearchFunction} from "./search"
-=======
+import { watchSearchFunction } from "./search";
 import { watchGetUserById } from "./userById";
 import { watchAttendEvent } from "./attend";
->>>>>>> 8b26dec438b185ae9d60fe721652aed1f3725e85
 
 export default function* rootSaga() {
   yield all([
@@ -29,12 +35,8 @@ export default function* rootSaga() {
     watchPutLikes(),
     watchDeletePosts(),
     watchUpdateAnnouncement(),
-<<<<<<< HEAD
     watchSearchFunction(),
-
-=======
     watchGetUserById(),
-    watchAttendEvent()
->>>>>>> 8b26dec438b185ae9d60fe721652aed1f3725e85
+    watchAttendEvent(),
   ]);
 }
